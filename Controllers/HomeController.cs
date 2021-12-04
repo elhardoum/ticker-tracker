fb218c24b82f3205b8cc2b9fd6d715a3bdf51f47
Salman Virji <salmanvirji@gmail.com>
2021-12-04 15:07:33 -0700

    Add /about page Fixes #9

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace TickerTracker.Controllers
         {
             return View();
         }
-
+          public IActionResult About()
+        {
+            return View();
+        }
         // @todo delete
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
